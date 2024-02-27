c77e7db4f540967f4641dfc594e9ca10a8e498ff
Patryk Gajek <pgajek94@gmail.com>
2024-02-27 13:11:34 +0100

    launches- model, router, controller and getAllLaunches method

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(planetsRouter);
+app.use(launchesRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
---
+++ server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+const launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: "Hakuna matata",
+  rocket: "Super Rocket VII",
+  launchDate: new Date("21-01-2029"),
+  destination: "Hakuna matatuj",
+  customers: ["NASA", "Dzika prasa", "Timon & Pumba"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+  launches,
+};
