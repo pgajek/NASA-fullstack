62c1affcf99724a967b4ccb79f0acc1bc4f94a20
Patryk Gajek <pgajek94@gmail.com>
2024-03-01 13:51:24 +0100

    mongo connection have been seted

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,26 @@
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
+const MONGO_URL =
+  "mongodb+srv://redooo25:redooo25@dashboard-nasa.mxpvd5z.mongodb.net/?retryWrites=true&w=majority&appName=Dashboard-nasa";
+// const MONGO_URL = process.env.MONGO_URL;
 
 const { loadPlanetsData } = require("./models/planets.model");
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
