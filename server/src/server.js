d3d26e88482ff03b8f7f42646b3886ff0c68cb4b
Patryk Gajek <pgajek94@gmail.com>
2024-03-02 13:52:27 +0100

    setup for testing mongoo

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,16 @@
 const http = require("http");
 const app = require("./app");
-const mongoose = require("mongoose");
+const { loadPlanetsData } = require("./models/planets.model");
+const { connectDatabase } = require("./services/mongo");
+
 require("dotenv").config();
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-const MONGO_URL = process.env.MONGO_URL;
-
-const { loadPlanetsData } = require("./models/planets.model");
-
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await connectDatabase();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
---
+++ server/src/services/mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+const connectDatabase = async () => {
+  await mongoose.connect(MONGO_URL);
+};
+
+module.exports = {
+  connectDatabase,
+};
